e9301d89f2ab40ec3a1c5eeb192bd15d7910a260
DEValdivieso <diego.valdivieso-devosinc@farmaciasperuanas.pe>
2025-07-08 11:18:14 -0500

    add first version

---
+++ src/test/java/pe/edu/diego/proyectospring/ProyectoSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.edu.diego.proyectospring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
