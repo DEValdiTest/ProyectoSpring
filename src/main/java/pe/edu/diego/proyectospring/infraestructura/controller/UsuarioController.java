18b9f3aa40c24fcc0c0f86bfd70833692857ca0a
DEValdivieso <omarjet30@gmail.com>
2025-09-30 12:50:06 -0500

    add api get usuario

---
+++ src/main/java/pe/edu/diego/proyectospring/infraestructura/controller/UsuarioController.java
@@ -0,0 +1,18 @@
+package pe.edu.diego.proyectospring.infraestructura.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import pe.edu.diego.proyectospring.domain.entity.Usuario;
+
+@RestController
+public class UsuarioController {
+
+    @GetMapping("/obtenerUsuario")
+    public Usuario obtenerUsuario() {
+        Usuario usuario = new Usuario();
+        usuario.setId(1);
+        usuario.setNombre("Usuario 1");
+
+        return usuario;
+    }
+}
